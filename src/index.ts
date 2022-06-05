8f02ee18835c92e05cf7a2ac957cb172c1ca0741
Guilherme Lopes Ferreira <gferreira525@gmail.com>
2022-06-05 16:36:12 -0300

    add nodemon

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World 3");
+});
+
+app.post("/", (req, res) => {
+  res.status(200).json({ Hello: "world" });
 });
 
 app.listen(port, () => console.log(`App running on port ${port}`));

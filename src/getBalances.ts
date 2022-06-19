00ef9802db196b346c1a4164e66a656fcf592afb
Guilherme Lopes Ferreira <gferreira525@gmail.com>
2022-06-19 16:39:21 -0300

    add link, eth and dot to tokenlist

--- src/getBalances.ts
+++ src/getBalances.ts
@@ -1,21 +1,11 @@
 import { getbalance, getERC20Balance } from "./getBalance";
 import getProvider from "./getProvider";
+import tokenList from "./tokenList";
 
 type Balance = {
   token: string;
   amount: any;
 };
-
-const tokenList = [
-  {
-    name: "btcb",
-    contract: "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c",
-  },
-  {
-    name: "busd",
-    contract: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
-  },
-];
 
 const getBalances = async (address: string): Promise<Balance[]> => {
   const provider = await getProvider();
---
+++ src/tokenList.ts
@@ -0,0 +1,22 @@
+export default [
+  {
+    name: "btcb",
+    contract: "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c",
+  },
+  {
+    name: "busd",
+    contract: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
+  },
+  {
+    name: "eth",
+    contract: "0x2170ed0880ac9a755fd29b2688956bd959f933f8",
+  },
+  {
+    name: "dot",
+    contract: "0x7083609fce4d1d8dc0c979aab8c869ea2c873402",
+  },
+  {
+    name: "link",
+    contract: "0xf8a0bf9cf54bb92f17374d9e9a321e6a111a51bd",
+  },
+];
